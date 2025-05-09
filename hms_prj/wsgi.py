e8c193abb298968d50989e6131904c58660186c5
MohamedRedaRadouani <radouanischool@gmal.com>
2025-05-10 00:11:36 +0100

    Serve MEDIA files via WhiteNoise

--- hms_prj/wsgi.py
+++ hms_prj/wsgi.py
@@ -14,3 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hms_prj.settings')
 
 application = get_wsgi_application()
+from whitenoise import WhiteNoise
+from django.conf import settings
+
+application = WhiteNoise(application, root=settings.STATIC_ROOT)  # serve /static/
+application.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)  # serve /media/
