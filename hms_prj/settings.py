e900f0b562c12824dbbe7a2180d437f929a41ef8
MohamedRedaRadouani <radouanischool@gmal.com>
2025-05-09 11:32:17 +0100

    Add WhiteNoise static file config

--- hms_prj/settings.py
+++ hms_prj/settings.py
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -443,4 +444,6 @@ CKEDITOR_5_CONFIGS = {
         },
     },
 }
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
