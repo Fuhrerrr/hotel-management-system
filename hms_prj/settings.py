3539c984c98c013bf9d0c8edb3e6719169ecadc0
MohamedRedaRadouani <radouanischool@gmal.com>
2025-05-09 11:25:56 +0100

    Set ALLOWED_HOSTS and DEBUG for deployment

--- hms_prj/settings.py
+++ hms_prj/settings.py
@@ -28,9 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m*t5wynyhd=2udczig6#n&0337+m=ga!p=cglnd-+srqdpq4r2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
